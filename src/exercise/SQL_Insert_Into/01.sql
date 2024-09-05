7c9399e6f05c5da18ca517871b476c34688c69ba
Haripreetam <haripreetam333@gmail.com>
2024-09-05 10:53:31 +0530

    [Haripreetam] ADD:how to use the insert into clause in sql

---
+++ src/exercise/SQL_Insert_Into/01.sql
@@ -0,0 +1,15 @@
+-- Insert a new product into the products table with a specified category and weight.
+INSERT INTO products (
+        product_id,
+        product_category_name,
+        product_weight_g
+    )
+VALUES (
+        '1e9e8ef04dbcff4541ed266577777775',
+        'clothing',
+        '43000'
+    );
+
+-- SELECT *
+-- FROM products
+-- WHERE product_id = '1e9e8ef04dbcff4541ed266577777775';
